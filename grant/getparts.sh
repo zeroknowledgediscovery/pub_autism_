77ae166424a8716a331c933ceb2af6dfdb765d22
ishanu chattopadhyay <ishanu.chattopadhyay@gmail.com>
2021-02-15 07:21:44 -0600

    rest

--- grant/getparts.sh
+++ grant/getparts.sh
@@ -2,7 +2,10 @@
 
 FILE=$1
 
-pdfjam $FILE 2 --letterpaper -o specific_aims.pdf
-pdfjam $FILE 3-8  --letterpaper -o strategy.pdf
-pdfjam $FILE 9-  --letterpaper -o reference.pdf
+pdfjam $FILE 1 --letterpaper -o summary.pdf
+pdfjam $FILE 2 --letterpaper -o narrative.pdf
+pdfjam $FILE 3-6 --letterpaper -o facilities.pdf
+pdfjam $FILE 8 --letterpaper -o specific_aims.pdf
+pdfjam $FILE 9-14  --letterpaper -o strategy.pdf
+pdfjam $FILE 15-  --letterpaper -o reference.pdf
 
